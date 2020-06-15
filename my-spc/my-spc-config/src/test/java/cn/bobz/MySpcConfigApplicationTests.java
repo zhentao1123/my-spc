0b85cc0e6eb35bcee617d7976c57d23f534d3e38
Bob Zhang <zzt1981@126.com>
2020-06-15 17:51:00 +0800

    create

---
+++ my-spc/my-spc-config/src/test/java/cn/bobz/MySpcConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bobz;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpcConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ my-spc/my-spc-eureka/src/main/java/cn/bobz/MySpcEurekaApplication.java
@@ -0,0 +1,13 @@
+package cn.bobz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpcEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpcEurekaApplication.class, args);
+	}
+
+}
