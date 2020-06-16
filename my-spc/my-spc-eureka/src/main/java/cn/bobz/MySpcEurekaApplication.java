6ae0a493ff17de732aaf0378f72a123fb293ba15
Bob Zhang <zzt1981@126.com>
2020-06-16 13:55:02 +0800

    update

--- my-spc/my-spc-eureka/src/main/java/cn/bobz/MySpcEurekaApplication.java
+++ my-spc/my-spc-eureka/src/main/java/cn/bobz/MySpcEurekaApplication.java
@@ -2,8 +2,10 @@ package cn.bobz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MySpcEurekaApplication {
 
 	public static void main(String[] args) {
