e0e4ae2e99939e391bd3749bd7deb6d7acdf63f0
Bob Zhang <zzt1981@126.com>
2020-06-16 17:49:33 +0800

    update

---
+++ my-spc/my-spc-admin/src/main/java/cn/bobz/MySpcAdminApplication.java
@@ -0,0 +1,18 @@
+package cn.bobz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class MySpcAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpcAdminApplication.class, args);
+	}
+
+}
