7fe1e49bfc3a674d594fd9b3c84a3d59a603b72d
Bob Zhang <zzt1981@126.com>
2020-06-17 16:08:30 +0800

    update

--- my-spc/my-spc-gateway/src/main/java/cn/bobz/MySpcGatewayApplication.java
+++ my-spc/my-spc-gateway/src/main/java/cn/bobz/MySpcGatewayApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableZuulProxy
 public class MySpcGatewayApplication {
 
 	public static void main(String[] args) {
