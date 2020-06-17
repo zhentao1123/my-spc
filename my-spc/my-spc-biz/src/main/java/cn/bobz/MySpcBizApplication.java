923e1d2248cf16ce806137e9a2e61cce4e4ea392
Bob Zhang <zzt1981@126.com>
2020-06-17 15:35:27 +0800

    update

---
+++ my-spc/my-spc-biz/src/main/java/cn/bobz/MySpcBizApplication.java
@@ -0,0 +1,13 @@
+package cn.bobz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpcBizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpcBizApplication.class, args);
+	}
+
+}
