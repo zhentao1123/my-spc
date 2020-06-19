e3110f8da3581306d448d7977a50a1b4099edd7d
Bob Zhang <zzt1981@126.com>
2020-06-19 16:36:35 +0800

    update

---
+++ my-spc/my-spc-turbine/src/main/java/cn/bobz/MySpcTurbineApplication.java
@@ -0,0 +1,21 @@
+package cn.bobz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableTurbine
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+public class MySpcTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpcTurbineApplication.class, args);
+	}
+
+}
