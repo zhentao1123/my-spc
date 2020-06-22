bb5b41d2df4121c4c34995a04bde0f4e80bbae8c
Bob Zhang <zzt1981@126.com>
2020-06-22 11:04:22 +0800

    upDATE

--- my-spc/my-spc-biz/src/main/java/cn/bobz/MySpcBizApplication.java
+++ my-spc/my-spc-biz/src/main/java/cn/bobz/MySpcBizApplication.java
@@ -1,5 +1,7 @@
 package cn.bobz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/biz")
 public class MySpcBizApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(MySpcBizApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MySpcBizApplication.class, args);
 	}
 
 	@RequestMapping(path = "/getUser")
 	public User getUser() {
+		log.info("==> Request getUser ");
 		User user = new User();
 		user.setName("Tom");
 		user.setAge(10);
