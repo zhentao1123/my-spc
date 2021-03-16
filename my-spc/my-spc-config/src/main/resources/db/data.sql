eb73cf8dc3b7b81db9ca7b79e84e2bdfd8803db5
ztzhang <zzt1981@126.com>
2021-03-16 15:36:23 +0800

    update

--- my-spc/my-spc-config/src/main/resources/db/data.sql
+++ my-spc/my-spc-config/src/main/resources/db/data.sql
@@ -4,24 +4,27 @@ VALUES ('demo', 'default', 'master', 'app.greet.name', 'Demo');
 -- eureka server
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
 VALUES ('eureka-server', 'default', 'master', 'server.port', '${SERVER_PORT:8010}');
+
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
 VALUES ('eureka-server', 'default', 'master', 'eureka.instance.preferIpAddress', 'true');
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
 VALUES ('eureka-server', 'default', 'master', 'eureka.instance.lease-renewal-interval-in-seconds', '10');
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
 VALUES ('eureka-server', 'default', 'master', 'eureka.instance.lease-expiration-duration-in-seconds', '30');
-INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
-VALUES ('eureka-server', 'default', 'master', 'eureka.client.registerWithEureka', 'true');
-INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
-VALUES ('eureka-server', 'default', 'master', 'eureka.client.fetchRegistry', 'true');
-INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
-VALUES ('eureka-server', 'default', 'master', 'eureka.client.serviceUrl.defaultZone', 'http://${EUREKA_SERVER_HOST}:${EUREKA_SERVER_PORT}/eureka/');
+
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
 VALUES ('eureka-server', 'default', 'master', 'eureka.server.waitTimeInMsWhenSyncEmpty', '0');
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
 VALUES ('eureka-server', 'default', 'master', 'eureka.server.enableSelfPreservation', 'false');
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
 VALUES ('eureka-server', 'default', 'master', 'eureka.server.eviction-interval-timer-in-ms', '10000');
+
+INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
+VALUES ('eureka-server', 'default', 'master', 'eureka.client.registerWithEureka', 'false'); //-- 注册自身
+INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
+VALUES ('eureka-server', 'default', 'master', 'eureka.client.fetchRegistry', 'false');
+INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
+VALUES ('eureka-server', 'default', 'master', 'eureka.client.serviceUrl.defaultZone', 'http://${EUREKA_SERVER_HOST:127.0.0.1}:${EUREKA_SERVER_PORT:8010}/eureka/');
 
 -- gateway
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
@@ -37,7 +40,7 @@ VALUES ('gateway', 'default', 'master', 'eureka.client.registerWithEureka', 'tru
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
 VALUES ('gateway', 'default', 'master', 'eureka.client.fetchRegistry', 'true');
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
-VALUES ('gateway', 'default', 'master', 'eureka.client.serviceUrl.defaultZone', 'http://${EUREKA_SERVER1_HOST}:${EUREKA_SERVER1_PORT}/eureka/');
+VALUES ('gateway', 'default', 'master', 'eureka.client.serviceUrl.defaultZone', 'http://${EUREKA_SERVER1_HOST:127.0.0.1}:${EUREKA_SERVER1_PORT:8010}/eureka/');
 
 -- turbine
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
@@ -55,7 +58,7 @@ VALUES ('turbine', 'default', 'master', 'eureka.client.registerWithEureka', 'tru
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
 VALUES ('turbine', 'default', 'master', 'eureka.client.fetchRegistry', 'true');
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
-VALUES ('turbine', 'default', 'master', 'eureka.client.serviceUrl.defaultZone', 'http://${EUREKA_SERVER1_HOST}:${EUREKA_SERVER1_PORT}/eureka/');
+VALUES ('turbine', 'default', 'master', 'eureka.client.serviceUrl.defaultZone', 'http://${EUREKA_SERVER1_HOST:127.0.0.1}:${EUREKA_SERVER1_PORT:8010}/eureka/');
 
 -- spring-admin
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
@@ -71,7 +74,7 @@ VALUES ('spring-admin', 'default', 'master', 'eureka.client.registerWithEureka',
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
 VALUES ('spring-admin', 'default', 'master', 'eureka.client.fetchRegistry', 'true');
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
-VALUES ('spring-admin', 'default', 'master', 'eureka.client.serviceUrl.defaultZone', 'http://${EUREKA_SERVER1_HOST}:${EUREKA_SERVER1_PORT}/eureka/');
+VALUES ('spring-admin', 'default', 'master', 'eureka.client.serviceUrl.defaultZone', 'http://${EUREKA_SERVER1_HOST:127.0.0.1}:${EUREKA_SERVER1_PORT:8010}/eureka/');
 
 -- biz-service
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
@@ -87,4 +90,4 @@ VALUES ('biz-service', 'default', 'master', 'eureka.client.registerWithEureka', 
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
 VALUES ('biz-service', 'default', 'master', 'eureka.client.fetchRegistry', 'true');
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
-VALUES ('biz-service', 'default', 'master', 'eureka.client.serviceUrl.defaultZone', 'http://${EUREKA_SERVER1_HOST}:${EUREKA_SERVER1_PORT}/eureka/');
+VALUES ('biz-service', 'default', 'master', 'eureka.client.serviceUrl.defaultZone', 'http://${EUREKA_SERVER1_HOST:127.0.0.1}:${EUREKA_SERVER1_PORT:8010}/eureka/');
