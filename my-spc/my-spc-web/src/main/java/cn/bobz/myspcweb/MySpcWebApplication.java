5f834145a568adb0f174acc5b116d092496d8528
ztzhang <zzt1981@126.com>
2021-03-17 09:35:48 +0800

    update

---
+++ my-spc/my-spc-web/src/main/java/cn/bobz/myspcweb/MySpcWebApplication.java
@@ -0,0 +1,13 @@
+package cn.bobz.myspcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpcWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpcWebApplication.class, args);
+    }
+
+}
