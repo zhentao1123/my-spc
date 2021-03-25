01a8fa241f39e6fce490ed91b6683511c6123b8d
ztzhang <zzt1981@126.com>
2021-03-25 13:31:33 +0800

    update

--- my-spc/my-spc-biz/src/main/java/cn/bobz/biz/MySpcBizApplication.java
+++ my-spc/my-spc-biz/src/main/java/cn/bobz/biz/MySpcBizApplication.java
@@ -1,13 +1,8 @@
-package cn.bobz;
+package cn.bobz.biz;
 
-import cn.bobz.module.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableFeignClients
