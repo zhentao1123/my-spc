6525764303cb74691bae0b8dd510cb47a98af18e
ztzhang <zzt1981@126.com>
2021-04-19 17:34:47 +0800

    update

--- my-spc/my-spc-nacos/src/main/java/cn/bobz/ConsumerController.java
+++ my-spc/my-spc-nacos/src/main/java/cn/bobz/ConsumerController.java
@@ -3,6 +3,7 @@ package cn.bobz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -28,5 +29,10 @@ public class ConsumerController {
     @RequestMapping(value = "/call-print", method = RequestMethod.POST)
     public String print(@RequestBody User user) {
         return restTemplate.postForObject("http://"+serviceProvider+"/print", user, String.class);
+    }
+
+    @RequestMapping(value = "/call-test", method = RequestMethod.GET)
+    public String test() {
+        return restTemplate.getForObject("http://"+serviceProvider+"/test", String.class);
     }
 }
--- my-spc/my-spc-nacos/src/main/java/cn/bobz/ProviderController.java
+++ my-spc/my-spc-nacos/src/main/java/cn/bobz/ProviderController.java
@@ -1,18 +1,32 @@
 package cn.bobz;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
+@RefreshScope
 @RestController
 public class ProviderController {
 
+    @Value("${test}")
+    private String test;
+
+    @Value("${test-share}")
+    private String testShare;
+
     @GetMapping(value = "/echo/{string}")
     public String echo(@PathVariable String string) {
-        return string;
+        return string ;
     }
 
     @PostMapping(value = "/print")
     public String print(@RequestBody User user) {
         return "name : " + user.getName() + " , age : " + user.getAge();
+    }
+
+    @GetMapping(value = "/test")
+    public String print() {
+        return test + " : " +testShare;
     }
 
 }
